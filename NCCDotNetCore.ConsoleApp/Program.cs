ce9ea9bd9a25f86c5a088a9a6a1e0f6f2515ca43
NayChiCho <naychicho.dev@gmail.com>
2024-06-04 21:27:15 +0630

    _Ado.Net[Read]

--- NCCDotNetCore.ConsoleApp/Program.cs
+++ NCCDotNetCore.ConsoleApp/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using System.Data.SqlClient;
+
+
+Console.WriteLine("Hello, World!");
+
+//npm
+//pub.dev
+//nuget
+//SqlClient
+
+
+
+//Ctrl+.
+//F10
+//F11
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = ".";//server name
+stringBuilder.InitialCatalog = "DotNetTrainingBatch4";//database name
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sasa@123";
+
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+
+
+string query = "select * from tbl_blog";
+SqlCommand commond= new SqlCommand(query, connection);
+SqlDataAdapter adapter = new SqlDataAdapter(commond);
+DataTable dt = new DataTable();
+adapter.Fill(dt);
+connection.Close();
+
+//dataset => datatable
+//datatable => datarow
+//datarow => datacolumn
+
+foreach(DataRow row in dt.Rows)
+{
+    Console.WriteLine("Blog Id => "+ row["BlogId"]);
+    Console.WriteLine("Blog Title => " + row["BlogTitle"]);
+    Console.WriteLine("Blog Author => " + row["BlogAuthor"]);
+    Console.WriteLine("Blog Conect => " + row["BlogContent"]);
+    Console.WriteLine("--------------------------------");
+}
+//ado.Net Read
+Console.ReadKey();
