c92da7bb3bf36047077fbe02074d5cba461f0a71
NayChiCho <naychicho.dev@gmail.com>
2024-06-10 18:51:18 +0630

    AdoDotNetRead[Complete]

---
+++ NCCDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCDotNetCore.ConsoleApp
+{
+    public class AdoDotNetExample
+    {
+        private readonly SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",//server name
+            InitialCatalog = "DotNetTrainingBatch4",//database name
+            UserID = "sa",
+            Password = "sasa@123"
+        };
+
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+
+
+            string query = "select * from tbl_blog";
+            SqlCommand commond = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(commond);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            connection.Close();
+            foreach (DataRow row in dt.Rows)
+            {
+                Console.WriteLine("Blog Id => " + row["BlogId"]);
+                Console.WriteLine("Blog Title => " + row["BlogTitle"]);
+                Console.WriteLine("Blog Author => " + row["BlogAuthor"]);
+                Console.WriteLine("Blog Conect => " + row["BlogContent"]);
+                Console.WriteLine("--------------------------------");
+            }
+        }
+        
+
+    }
+}
--- NCCDotNetCore.ConsoleApp/Program.cs
+++ NCCDotNetCore.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using NCCDotNetCore.ConsoleApp;
+using System.Data;
 using System.Data.SqlClient;
 
 
@@ -15,33 +16,35 @@ Console.WriteLine("Hello, World!");
 //F10
 //F11
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = ".";//server name
-stringBuilder.InitialCatalog = "DotNetTrainingBatch4";//database name
-stringBuilder.UserID = "sa";
-stringBuilder.Password = "sasa@123";
+//SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+//stringBuilder.UserID = "sa";
+//stringBuilder.Password = "sasa@123";
 
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+//SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
 
 
-string query = "select * from tbl_blog";
-SqlCommand commond= new SqlCommand(query, connection);
-SqlDataAdapter adapter = new SqlDataAdapter(commond);
-DataTable dt = new DataTable();
-adapter.Fill(dt);
-connection.Close();
+//string query = "select * from tbl_blog";
+//SqlCommand commond= new SqlCommand(query, connection);
+//SqlDataAdapter adapter = new SqlDataAdapter(commond);
+//DataTable dt = new DataTable();
+//adapter.Fill(dt);
+//connection.Close();
 
 //dataset => datatable
 //datatable => datarow
 //datarow => datacolumn
 
-foreach(DataRow row in dt.Rows)
-{
-    Console.WriteLine("Blog Id => "+ row["BlogId"]);
-    Console.WriteLine("Blog Title => " + row["BlogTitle"]);
-    Console.WriteLine("Blog Author => " + row["BlogAuthor"]);
-    Console.WriteLine("Blog Conect => " + row["BlogContent"]);
-    Console.WriteLine("--------------------------------");
-}
+//foreach(DataRow row in dt.Rows)
+//{
+ //   Console.WriteLine("Blog Id => "+ row["BlogId"]);
+   // Console.WriteLine("Blog Title => " + row["BlogTitle"]);
+   //Console.WriteLine("Blog Author => " + row["BlogAuthor"]);
+   //Console.WriteLine("Blog Conect => " + row["BlogContent"]);
+   //Console.WriteLine("--------------------------------");
+//}
 //ado.Net Read
+
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Read();
 Console.ReadKey();
