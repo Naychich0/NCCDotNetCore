f38198488fc8c99d683bc7ca62c48bb3d738c4dc
NayChiCho <naychicho.dev@gmail.com>
2024-06-18 00:15:02 +0630

    ApiModelRename

--- NCCDotNetCore.RestApi/Db/AppDbContext.cs
+++ NCCDotNetCore.RestApi/Db/AppDbContext.cs
@@ -1,19 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NCCDotNetCore.ConsoleApp.Dtos;
+using NCCDotNetCore.RestApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NCCDotNetCore.RestApi.Db
+namespace NCCDotNetCore.RestApi.Db;
+
+internal class AppDbContext : DbContext
 {
-    internal class AppDbContext : DbContext
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
-        }
-        public DbSet<BlogDto> Blogs { get; set; }
+        optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
     }
+    public DbSet<BlogModel> Blogs { get; set; }
 }
